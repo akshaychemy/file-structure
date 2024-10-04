ced82548b29ac750e7f48f4b9d45a98d05c68a06
Krishna Mayekar <krishna.mayekar@aithinkers.com>
2024-10-04 21:45:40 +0530

    deleted file

--- backend/create_db.py
+++
@@ -1,8 +0,0 @@
-# create_db.py
-from src.config.config import create_app, db
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()  # This creates all tables defined in your models
-    print("Tables created or already exist.")
